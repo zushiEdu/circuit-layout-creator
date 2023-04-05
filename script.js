2fb78da20eccc70609b88603dc54cacd28c1d034
Ethan <hubee8329@wrdsb.ca>
2023-04-05 15:02:07 -0400

    Instruction Interpreter

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+class child {
+    constructor(type, name) {
+        this.type = type;
+        this.name = name;
+    }
+}
+
+class circuit {
+    constructor(circuitProperty, components) {
+        this.circuitProperty = circuitProperty;
+        this.components = components;
+    }
+
+    addComponent(component) {
+        this.components.push(component);
+    }
+}
+
+class component {
+    constructor(componentProperty, children) {
+        this.componentProperty = componentProperty;
+        this.children = children;
+    }
+
+    addChild(child) {
+        this.children.push(child);
+    }
+}
+
+class componentProperties {
+    constructor(height, width, name, type, value, colors,) {
+        this.height = height;
+        this.width = width;
+        this.name = name;
+        this.colors = colors;
+        this.value = value;
+        this.type = type;
+    }
+}
+
+class circuitProperties {
+    constructor(name, description, author, doc, dole) {
+        this.name = name;
+        this.description = description;
+        this.author = author;
+        this.doc = doc;
+        this.dole = dole;
+    }
+}
+
 function rgb(r, g, b) {
     this.r = r;
     this.g = g;
@@ -11,17 +61,171 @@ const zoom = document.getElementById("zoom-select");
 const height = window.innerHeight - 40;
 const width = window.innerWidth * 0.75;
 
-const defaultGridSize = 25;
+const defaultGridSize = 30;
+var calcualtedZoom = defaultGridSize * zoom.value;
 
 painter.canvas.width = width;
 painter.canvas.height = height;
 
+drawGrid(0, 0, width, height, calcualtedZoom, new rgb(51, 51, 51));
 
-drawGrid(0, 0, width, height, 25 * zoom.value, new rgb(51, 51, 51));
+var led_board = new circuit(
+    new circuitProperties(
+        "Led Board",
+        "Led Simply On",
+        "Ethan Huber", "2023-04-04",
+        "2023-04-04"
+    ),
+    [
+        new component(
+            new componentProperties(
+                `5T`,
+                `5T`,
+                `Base Board`,
+                `board`,
+                null,
+                [
+                    `#fff`
+                ]
+            )
+            ,
+            new child("diode", "LED1")
+        ),
+        new component(
+            new componentProperties(
+                `0.9U`,
+                `0.9U`,
+                `LED1`,
+                `diode-led`,
+                null,
+                [
+                    `#fff`
+                ]
+            ),
+            null
+        )
+    ]
+)
+
+//console.log(led_board);
+
+drawType(`diode-led`, null, "5T", "5T");
+
+pinInstruction(220, 220, 220, "L");
+pinInstruction(220, 220, 220, "R");
+rectInstruction(255, 0, 0, "C");
+circleInstruction(255, 0, 0, "U");
+
+function circleInstruction(r, g, b, location) {
+    x = 5 * calcualtedZoom;
+    y = 5 * calcualtedZoom;
+
+    var step = calcualtedZoom / 8;
+
+    painter.fillStyle = `rgb(${r},${g},${b})`;
+    painter.beginPath();
+    switch (location) {
+        case "C":
+            painter.ellipse(x + step * 4, y + step * 4, step * 3, step * 3, 0, 0, 360);
+            break;
+        case "L":
+            painter.ellipse(x + step * 3, y + step * 4, step * 3, step * 3, 0, 0, 360);
+            break;
+        case "R":
+            painter.ellipse(x + step * 5, y + step * 4, step * 3, step * 3, 0, 0, 360);
+            break;
+        case "U":
+            painter.ellipse(x + step * 4, y + step * 3, step * 3, step * 3, 0, 0, 360);
+            break;
+        case "D":
+            painter.ellipse(x + step * 4, y + step * 5, step * 3, step * 3, 0, 0, 360);
+            break;
+    }
+    painter.fill();
+}
+
+function rectInstruction(r, g, b, location) {
+    x = 5 * calcualtedZoom;
+    y = 5 * calcualtedZoom;
+
+    var step = calcualtedZoom / 8;
+
+    painter.fillStyle = `rgb(${r},${g},${b})`;
+    painter.beginPath();
+    switch (location) {
+        case "C":
+            painter.rect(x + step * 1, y + step * 2, step * 6, step * 4);
+            break;
+    }
+    painter.fill();
+}
+
+function pinInstruction(r, g, b, location) {
+    x = 5 * calcualtedZoom;
+    y = 5 * calcualtedZoom;
+
+    var step = calcualtedZoom / 8;
+
+    painter.fillStyle = `rgb(${r},${g},${b})`;
+    painter.beginPath();
+    switch (location) {
+        case "L":
+            painter.rect(x + step * 2, y + step * 5, step * 1, step * 3);
+            break;
+        case "R":
+            painter.rect(x + step * 5, y + step * 5, step * 1, step * 3);
+            break;
+    }
+    painter.fill();
+}
+
+function drawType(type, colors, gridX, gridY, posX, posY) {
+    type = "instructions/" + type.replace("-", "/") + ".txt";
+    fetch(type)
+        .then((response) => response.text())
+        .then((text) => {
+            const lines = text.split("\n");
+
+            var run = true;
+            var lineNumber = 0;
+
+            // Extract Drawing Mode
+            var drawMode;
+            if (gridX[gridX.length] == "T" && gridY[gridY.length] == "T") {
+                drawMode = `tile`;
+
+            } else {
+                drawMode = `single`
+            }
+            gridX = gridX.substr(0, gridX.length - 1);
+            gridY = gridY.substr(0, gridY.length - 1);
+
+            // Check For Lines Instruction
+            while (run) {
+                var instruction = lines[lineNumber].split(" ");
+                switch (instruction[0]) {
+                    case "C":
+                        // Defines Circle Properties
+                        break;
+                    case "*":
+                        // End Of File Symbol
+                        run = false;
+                        break;
+                }
+
+                lineNumber++;
+            }
+        });
+}
 
 function updateZoom() {
     clearGrid();
-    drawGrid(0, 0, width, height, 25 * zoom.value, new rgb(51, 51, 51));
+    calcualtedZoom = defaultGridSize * zoom.value;
+    drawGrid(0, 0, width, height, calcualtedZoom, new rgb(51, 51, 51));
+    pinInstruction(220, 220, 220, "L");
+    pinInstruction(220, 220, 220, "R");
+    rectInstruction(255, 0, 0, "C");
+    circleInstruction(255, 0, 0, "U");
 }
 
 function clearGrid() {
